88e28b8ffadbf6c3cf21202fab18adc522d27323
Oladokun Oladapo <oladokun104@gmail.com>
2020-10-04 10:48:17 +0100

    Start Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -82,14 +82,14 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main, menu);
         return true;
     }
+
+
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         Intent intent = new Intent(this, InfoActivity.class);
         startActivity(intent);
         return super.onOptionsItemSelected(item);
